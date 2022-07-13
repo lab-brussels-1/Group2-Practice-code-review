f537279cda40a316d5ccd457688dd1cd46c1325b
NazgulM <maniyasova.n@gmail.com>
2022-07-13 17:02:41 +0200

    deep-flat with import and export

--- src/deep-flat/deep-flat2.spec.mjs
+++ src/deep-flat/deep-flat2.spec.mjs
@@ -1,24 +1,4 @@
-/** ..........
- * Converts a nested array into a single array with no nesting.
- *
- * It returns a new array and there are no side-effects.
- *
- * @param {Array} [array=[]] - The array to flatten.
- * @returns {Array} Returns the new flattened array.
- * @example
- *
- * deepFlat([1, [2, [3, [4]], 5]]);
- * // -> [1, 2, 3, 4, 5]
- *
- * @example
- *
- * deepFlat(['a', ['b', [['c'], ['d']], 'e']]);
- * // -> ['a', 'b', 'c', 'd', 'e']
- */
-
-const deepFlat = (array = []) => {
-  return array.flat(Infinity);
-};
+import { deepFlat } from './deepflat.js';
 
 for (const solution of [deepFlat]) {
   describe(`${solution.name}: _`, () => {
---
+++ src/deep-flat/deepflat.js
@@ -0,0 +1,3 @@
+export const deepFlat = (array = []) => {
+  return array.flat(Infinity);
+};
