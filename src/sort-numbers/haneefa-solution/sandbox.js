8777fee4474aee15407e18a5454a87dcbf576e03
Haneefa Shaik <haneefa506@gmail.com>
2022-07-13 23:20:26 +0200

    Adding haneefa solution

---
+++ src/sort-numbers/haneefa-solution/sandbox.js
@@ -0,0 +1,23 @@
+/**
+ * Sorts an array of numbers from smallest to largest to largest.
+ *
+ * Returns a new array without modifying the original array.
+ *
+ * Does not need to support: NaN, Infinity, -Infinity.
+ *
+ * @param {number[]} [arrayOfNumbers=[]] - the array of numbers to sort
+ * @returns {number[]} a new array with the same numbers, but sorted
+ * @example
+ *
+ * sortNumbers([1.5, 1, -1.5, 0, -1]);
+ * // -> [-1.5, -1, 0, 1, 1.5]
+ */
+
+const sortingNumbers = (array = []) => {
+    const newArray = [...array];
+    newArray.sort((a, b) => {
+      return a - b;
+    });
+  
+    return newArray;
+  };
