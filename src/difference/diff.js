1556c56edc2bb8ad989db06cf9d8a6cb6a7415e0
Shewitttt <89597132+Shewitttt@users.noreply.github.com>
2022-07-21 14:08:35 +0200

    lint error fix

--- src/difference/diff.js
+++ src/difference/diff.js
@@ -3,31 +3,27 @@
 // iterate if there is are common elements.
 // If included  remove it if not continue till the array length.
 
-const array2D = [
-  [1, 2, 1],
-  [2, 8],
-];
-
-let arr1 = [];
 // changing the array to set to remove duplicate elements and convert it back to array.
 // set only allows unique elements.
+
 const removeDuplicates = (arr1) => {
-  const setArr1 = new Set(arr1);
-  arr1 = Array.from(setArr1);
-  return arr1;
+  let array2D = arr1;
+  const setArr1 = new Set(array2D);
+  array2D = Array.from(setArr1);
+  return array2D;
 };
 
 const findDifference = (arr1) => {
-  arr1[0] = removeDuplicates(arr1[0]);
-  arr1[1] = removeDuplicates(arr1[1]);
-  const firstArr = arr1[0];
-  for (let i = 0; i < arr1[1].length; i++) {
-    if (firstArr.includes(arr1[1][i])) {
+  const array2D = arr1;
+  array2D[0] = removeDuplicates(array2D[0]);
+  array2D[1] = removeDuplicates(array2D[1]);
+  for (let i = 0; i < array2D[1].length; i++) {
+    if (array2D[0].includes(arr1[1][i])) {
       // find the indexOf the common element found
-      const index = firstArr.indexOf(arr1[1][i]);
+      const index = array2D[0].indexOf(array2D[1][i]);
       // remove the element found
-      firstArr.splice(index, 1);
+      array2D[0].splice(index, 1);
     }
   }
-  return firstArr;
+  return array2D[0];
 };
