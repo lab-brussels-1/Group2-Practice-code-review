2309f3e5191c17662ca6977e386b245405a6f840
Shewitttt <89597132+Shewitttt@users.noreply.github.com>
2022-07-21 13:23:28 +0200

    difference solution added

---
+++ src/difference/diff.js
@@ -0,0 +1,33 @@
+// if there are duplicate arrays within an array it should be removed.
+// use a for loop where the 1st index in the array is iterated over the 0 index array elements and checked if each it is included or not. If included  remove it if not continue till the array length.
+//
+
+let array2D = [
+  [1, 2, 1],
+  [2, 8],
+];
+
+// changing the array to set to remove duplicate elements and convert it back to array.
+// set only allows unique elements.
+const removeDuplicates = (arr1) => {
+  let setArr1 = new Set(arr1);
+  arr1 = Array.from(setArr1);
+  return arr1;
+};
+
+const findDifference = (arr1) => {
+  arr1[0] = removeDuplicates(arr1[0]);
+  arr1[1] = removeDuplicates(arr1[1]);
+  let firstArr = arr1[0];
+  for (let i = 0; i < arr1[1].length; i++) {
+    if (firstArr.includes(arr1[1][i])) {
+      // find the indexOf the common element found
+      let index = firstArr.indexOf(arr1[1][i]);
+      //remove the element found
+      firstArr.splice(index, 1);
+    }
+  }
+  return firstArr;
+};
+
+console.log(findDifference(array2D));
---
+++ src/difference/diff.spec.js
@@ -0,0 +1,80 @@
+// functions
+
+const removeDuplicates = (arr1) => {
+  let setArr1 = new Set(arr1);
+  arr1 = Array.from(setArr1);
+  return arr1;
+};
+
+const findDifference = (arr1) => {
+  arr1[0] = removeDuplicates(arr1[0]);
+  arr1[1] = removeDuplicates(arr1[1]);
+  let firstArr = arr1[0];
+  for (let i = 0; i < arr1[1].length; i++) {
+    if (firstArr.includes(arr1[1][i])) {
+      // find the indexOf the common element found
+      let index = firstArr.indexOf(arr1[1][i]);
+      //remove the element found
+      firstArr.splice(index, 1);
+    }
+  }
+  return firstArr;
+};
+
+//test
+
+describe('Difference between two arrays', () => {
+  it('it should return [1] when [[1,2],[2,8]] are passed', () => {
+    expect(
+      findDifference([
+        [1, 2],
+        [2, 8],
+      ]),
+    ).toEqual([1]);
+  });
+  it('it should return [5,6] when [[5,4,6],[0,12,4]] are passed', () => {
+    expect(
+      findDifference([
+        [5, 4, 6],
+        [0, 12, 4],
+      ]),
+    ).toEqual([5, 6]);
+  });
+  it('it should return [10,20,5] when [[10,9,20,-2,5,0],[-2,0,9,18]] are passed', () => {
+    expect(
+      findDifference([
+        [10, 9, 20, -2, 5, 0],
+        [-2, 0, 9, 18],
+      ]),
+    ).toEqual([10, 20, 5]);
+  });
+});
+
+//Edge case- same two sets and disjoint arrays, empty arrays
+describe('Edge cases for difference between two arrays', () => {
+  it('it should return [] when [[1,2,3],[1,2,3]] are passed', () => {
+    expect(
+      findDifference([
+        [1, 2, 3],
+        [1, 2, 3],
+      ]),
+    ).toEqual([]);
+  });
+  it('it should return [4,5,6] when [[4,5,6],[7,8,9]] are passed', () => {
+    expect(
+      findDifference([
+        [4, 5, 6],
+        [7, 8, 9],
+      ]),
+    ).toEqual([4, 5, 6]);
+  });
+  it('it should return [] when [[],[10,20]] are passed', () => {
+    expect(findDifference([[], [10, 20]])).toEqual([]);
+  });
+  it('it should return [30,40] when [[30,40],[]] are passed', () => {
+    expect(findDifference([[30, 40], []])).toEqual([30, 40]);
+  });
+  it('it should return [] when [[],[]] are passed', () => {
+    expect(findDifference([[], []])).toEqual([]);
+  });
+});
